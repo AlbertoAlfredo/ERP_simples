3d2bb55ac3a38d54e1535dcf4bbbaf85a5787ade
AlbertoAlfredo <raioalfredo@gmail.com>
2019-01-27 19:52:49 -0200

    Base do cadastro de clientes pronto

--- cliente/urls.py
+++ cliente/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import cliente_cadastro
+from .views import cliente_cadastro, cliente_lista, cliente_editar, cliente_delete
 
 urlpatterns = [
-    path('cadastro/', cliente_cadastro),
+    path('cadastro/', cliente_cadastro, name="cliente_cadastro"),
+    path('lista/', cliente_lista, name="cliente_lista"),
+    path('editar/<int:id>/', cliente_editar, name="cliente_editar"),
+    path('delete/<int:id>/', cliente_delete, name='cliente_delete'),
 ]
+
--- cliente/views.py
+++ cliente/views.py
@@ -1,7 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Person
+from .forms import PersonForm
+from django.core.paginator import Paginator, InvalidPage
 
 
 
 # Create your views here.
+def cliente_lista(request):
+    clientes = Person.objects.all()
+    paginator = Paginator(clientes, 10)
+    page = request.GET.get('page')
+    clientes = paginator.get_page(page)
+    return render(request, 'cliente_lista.html', {'clientes':clientes})
+
+
 def cliente_cadastro(request):
-    return render(request, 'cliente_cadastro.html')
+    form = PersonForm(request.POST, request.FILES or None)
+
+    if form.is_valid():
+        form.save()
+        return redirect('cliente_lista')
+    return render(request, 'cliente_cadastro.html', {'form':form})
+
+def cliente_editar(request, id):
+    cliente = get_object_or_404(Person, pk=id)
+    form = PersonForm(request.POST or None, request.FILES or None, instance=cliente)
+
+    if form.is_valid():
+        form.save()
+        return redirect('cliente_lista')
+    return render(request, 'cliente_editar.html', {'form':form})
+
+def cliente_delete(request, id):
+    cliente = get_object_or_404(Person, pk=id)
+
+    if request.method == 'POST':
+        cliente.delete()
+        return redirect('cliente_lista')
+    return render(request, 'cliente_delete.html', {'cliente':cliente})
