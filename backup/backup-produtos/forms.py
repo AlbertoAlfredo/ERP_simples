aa794427da9bc6a75ed62c962ea5a59b7408b285
AlbertoAlfredo <raioalfredo@gmail.com>
2019-02-08 22:29:14 -0200

    retirado backup antigos, deixado apenas o futuro front end

--- backup/backup-produtos/forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm
-from .models import Veiculos
-
-
-class VeiculosForm(ModelForm):
-    class Meta:
-        model = Veiculos
-        fields = ['placa', 'marca', 'modelo', 'preco', 'etc', 'proprietario', 'foto']
--- backup/backup-produtos/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-from .views import veiculos_lista, veiculos_cadastro
-
-urlpatterns =[
-    path('', veiculos_lista, name = 'veiculos'),
-    path('cadastro/', veiculos_cadastro, name = 'cadastro' )
-]
